d16aca862b723cf5d9786ba2cf74d26cb4176baf
yourusername <shagorpixel@gmail.com>
2025-09-09 11:33:25 +0600

    Setup Client and admin React Project

---
+++ Admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2 className=' text-lg text-amber-600'>Hello World</h2>
+    </div>
+  );
+};
+
+export default App;
---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2 className=' text-lg font-medium text-center'>Hello world</h2>
+    </div>
+  );
+};
+
+export default App;
